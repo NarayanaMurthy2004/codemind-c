1030980377f43482c900f0a0ed16b88d052df965
NarayanaMurthy2004 <147488413+NarayanaMurthy2004@users.noreply.github.com>
2023-12-30 08:31:20 +0530

    #include<stdio.h> int main() {     int n,i,c=0,avg,t=0;     scanf("%d",&n);     int arr[n];     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);         t=t+arr[i];     }        avg=t/n;      for(i=0;i<n;i++)      {          if(arr[i]<=avg)          {              c++;          }      }      printf("%d",c); }

---
+++ Elements_smaller_than_average.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,c=0,avg,t=0;
+    scanf("%d",&n);
+    int arr[n];
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+        t=t+arr[i];
+    }
+       avg=t/n;
+     for(i=0;i<n;i++)
+     {
+         if(arr[i]<=avg)
+         {
+             c++;
+         }
+     }
+     printf("%d",c);
+}
